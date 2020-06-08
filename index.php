7249ab09b00a551d3410b2e8fb1e795901ab12d0
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 13:48:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 3 exercice 1</title>
+</head>
+<body>
+    <p>
+        <?php
+            for ($increment = 0; $increment <= 10; $increment++) {
+                echo "<p>$increment</p>";
+            }
+        ?>
+    </p>
+</body>
+</html>
